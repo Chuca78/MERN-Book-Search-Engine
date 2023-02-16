7df869536db8877dd422d57af0d4ce4641ed6860
Chuca <Chuca78@users.noreply.github.com>
2023-02-16 18:06:28 -0500

    replaced switch with routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 // todo: Create an Apollo Provider to make every request work with the Apollo Server.
 
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // replace with line below if not using switch
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
@@ -30,20 +30,20 @@ function App() {
       <Router>
         <>
           <Navbar />
-          <Switch>
+          {/* <Switch>
             <Route exact path="/" component={SearchBooks} />
             <Route exact path="/saved" component={SavedBooks} />
             <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
-          </Switch>
+          </Switch> */}
           {/* todo: double check - this might be able to replace switch */}
-          {/* <Routes>
-          <Route path="/" element={<SearchBooks />} />
-          <Route path="/saved" element={<SavedBooks />} />
-          <Route
-            path="*"
-            element={<h1 className="display-2">Wrong page!</h1>}
-          />
-        </Routes> */}
+          <Routes>
+            <Route path="/" element={<SearchBooks />} />
+            <Route path="/saved" element={<SavedBooks />} />
+            <Route
+              path="*"
+              element={<h1 className="display-2">Wrong page!</h1>}
+            />
+          </Routes>
         </>
       </Router>
     </ApolloProvider>
