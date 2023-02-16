4977d97576f3ef0b5f16d24ddfe2c17d3828f498
Chuca <Chuca78@users.noreply.github.com>
2023-02-16 16:59:53 -0500

    refactored App.js for apollo provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,52 @@
 // todo: Create an Apollo Provider to make every request work with the Apollo Server.
 
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// replace with line below if not using switch
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      // populates the header session that will communicate with Apollo
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
+});
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Routes>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={SearchBooks} />
+            <Route exact path="/saved" component={SavedBooks} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Switch>
+          {/* todo: double check - this might be able to replace switch */}
+          {/* <Routes>
           <Route path="/" element={<SearchBooks />} />
           <Route path="/saved" element={<SavedBooks />} />
           <Route
             path="*"
             element={<h1 className="display-2">Wrong page!</h1>}
           />
-        </Routes>
-      </>
-    </Router>
+        </Routes> */}
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
