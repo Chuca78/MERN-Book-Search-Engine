2af1b8c11cf335d81f5c7d4c02f071e50fd0f38a
Chuca <Chuca78@users.noreply.github.com>
2023-02-23 20:37:02 -0500

    modified for heroku MongoDB connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,20 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: true,
-});
+// this in the mongoDB connection for localhost
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: true,
+// });
+
+// todo: use this the format for the heroku connection to MongoDB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/your-database-name",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 module.exports = mongoose.connection;
